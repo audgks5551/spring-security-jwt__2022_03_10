09c484e38430ea4a899b998fafa31a975ce4dc71
audgks5551 <audgks5551@daum.net>
2022-03-10 17:58:35 +0900

    의존성 추가

---
+++ src/test/java/mark/oauth2Jwt/Oauth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package mark.oauth2Jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
